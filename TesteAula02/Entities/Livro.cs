edbba9054ee61e5d875b729ce52cb0bb99aaa092
Joao Manoel <beckerjoao102@gmail.com>
2022-03-16 19:50:07 -0300

    Teste01

---
+++ TesteAula02/Entities/Livro.cs
@@ -0,0 +1,32 @@
+namespace TesteAula02.Entities
+{
+    public class Livro
+    {
+        private int id;
+        string titulo;
+        decimal valor = 0;
+
+        public Autor Autor{ get; set; }
+
+        public Livro(int id, string titulo , Autor autor) {
+            this.id = id;
+            this.titulo = titulo;
+            this.Autor = autor;
+        }
+
+        public int GetId(){
+            return this.id;
+        }
+        public string GetTitulo(){
+            return this.titulo;
+        }
+        public decimal Valor{ get; set; } 
+        public decimal GetValor(){
+            return this.valor;
+        }
+
+        public void SetValor(decimal valor){
+            this.valor = valor;
+        }
+    }
+}
---
+++ TesteAula02/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using TesteAula02.Entities;
+
+namespace TesteAula02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Autor oAutor = new Autor(10,"João Manoel");
+            Livro objLivro = new Livro(100,"C# POO",oAutor);
+
+            Console.WriteLine(
+                objLivro.GetId()+" - "+
+                objLivro.GetTitulo()+ " - "+
+                objLivro.GetValor()+ " Autor: "+
+                objLivro.Autor.Nome);
+            
+            objLivro.SetValor(199);
+            Console.WriteLine("valor do livro: " + objLivro.GetValor());
+
+        }
+    }
+}
